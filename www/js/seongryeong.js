d547fbc3ba5a24308c390f94094b526182538c9a
aldegad <aldegad@naver.com>
2017-04-08 03:17:48 +0900

    0312end

--- www/js/seongryeong.js
+++ www/js/seongryeong.js
@@ -9,11 +9,14 @@
         select_box: {
             $target: null, // input
             $input : null,
-            focus: function () {
+            focus: function (e) {
                 var $this = $(this),
                     $target = $this.find('input'),
                     $children = $this.find('li'),
-                    $item = $children.find('li');
+                    $list = $this.find('ul'),
+                    $item = $children.find('li'),
+                    _ch = 0,
+                    _ctop = 0;
                 
                 //포커스된 타겟 지정
                 ffany.select_box.$target = $this;
@@ -45,6 +48,37 @@
                 $target.addClass('active').siblings().removeClass();
             }
         },
+        //input file img
+        file_preview: {
+            $img: null,
+            click: function () {
+                var $this = $(this),
+                    $input = $this.find('input'),
+                    $img = $this.find('img');
+                
+                ffany.file_preview.$img = $img;
+
+                $input.off('change', ffany.file_preview.change);
+                $input.on('change', ffany.file_preview.change);
+            },
+            change: function () {
+                var $input = $(this),
+                    $img = ffany.file_preview.$img;
+
+                if ($input[0].files && $input[0].files[0]) {
+                    var reader = new FileReader();
+
+                    reader.onload = function (e) {
+                        //이미지 리사이징
+                        $img.attr('src', e.target.result);
+                    };
+                    reader.readAsDataURL($input[0].files[0]);
+                } else {
+                    $input.val('');
+                    $img.attr('src', 'img/noimage.png');
+                }
+            }
+        },
         //detect click target
         target: function (e) {
             var touch = e.touches && e.touches[0],
@@ -59,6 +93,7 @@
         .on('focus', '.s_selectWrap', ffany.select_box.focus)
         .on('focusout', '.s_selectWrap', ffany.select_box.focusout)
         //radio btn
-        .on('click', '.s_radio_btn', ffany.radio_btn.click);
-    
+        .on('click', '.s_radio_btn', ffany.radio_btn.click)
+        //input file preview
+        .on('click', '.s_profile_upload', ffany.file_preview.click);
 }(window));
